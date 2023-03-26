99d05285ffcd56815d91f5d43bd81cd218c1388b
darshan <darshanDevrai@users.noreply.github.com>
2023-03-26 15:39:32 +0530

    removed firebase creds, deleted firebase test app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   // add your firebase config below
   firebase :  {
-    apiKey: "AIzaSyBMeO3BYgJXByci0u1zIoqfP9zSdyQaBgs",
-    authDomain: "ngelementcomments-65c13.firebaseapp.com",
-    projectId: "ngelementcomments-65c13",
-    storageBucket: "ngelementcomments-65c13.appspot.com",
-    messagingSenderId: "1066144449401",
-    appId: "1:1066144449401:web:3fa70cf133f13eb47d65a8"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
